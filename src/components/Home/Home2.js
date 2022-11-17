d142677f84243734b6bc33887bef8233fe6a59d7
Matteo <thebravematteo33@protonmail.com>
2022-11-16 21:12:50 -0300

    Feat/matteo-portfolio/fixing some words which was wrong

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,38 +19,29 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              Programming is like learning a different language. Once you start 
+              it's hard to assimilate words and things with your native language, but when you practice,
+              your fluency gets better and before you notice you are speaking fluently, that's why i love programming.
               <br />
-              <br />I am fluent in classics like
+              <br />I am deep learner of front-end and focus on
               <i>
-                <b className="purple"> C++, Javascript and Python. </b>
+                <b className="purple"> JavaScript  </b>
+                and frameworks 
+                 like
+                 <b className="purple"> React and NextJS</b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Deep Learning and Natural Launguage Processing.
-                </b>
+              My field of interest is creating new solutions&nbsp;
+              <i>and
+                <b className="purple"> bring it to life </b> 
+                using Web Frameworks and technologies{" "}
+              
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
-            </p>
+              When i have some free time i use my time to study new technologies and frameworks to sharpen my<b className="purple"> Axe</b>
+              </p>
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
