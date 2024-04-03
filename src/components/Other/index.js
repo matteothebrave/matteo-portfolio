80cc8ea3bec0ec30cb61f46205ab487e6da04c58
Matheus Grossi <thebravematteo33@protonmail.com>
2024-04-03 12:04:20 +0200

    commiting some stuff

--- src/components/Other/index.js
+++ src/components/Other/index.js
@@ -5,27 +5,32 @@ import Tilt from "react-parallax-tilt";
 import Particle from "../Particle";
 
 
+
 function Other() {
+ 
   return (
-  
+    
+      
     <Container fluid className="home-about-section" id="about">
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
-           <h1>FILMES SERAO AQUI</h1>
+           <h1>Eu a patroa e as crianças</h1>
+          <div>
+          <iframe src='https://widgets.sociablekit.com/twitch-live-videos/iframe/25392232' frameborder='0' width='100%' height='1000'></iframe>
+          </div>
+          {/* <h1>Todo mundo odeia o Chris</h1>
+          <iframe src='https://widgets.sociablekit.com/twitch-live-videos/iframe/25392238' frameborder='0' width='100%' height='1000'></iframe> */}
           </Col>
-          <Col md={4} className="myAvatar">
+          {/* <Col md={4} className="myAvatar">
             <Tilt>
               <img src={aboutMe} className="img-fluid" alt="avatar"
               style={{ height: "200px" }} />
             </Tilt>
-          </Col>
+          </Col> */}
         </Row>
         <Row>
-          <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
-       
-          </Col>
+         
         </Row>
       </Container>
     </Container>
