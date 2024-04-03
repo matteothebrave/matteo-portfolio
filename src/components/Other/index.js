3a02fb333d3ad1097179b602e40920431027c035
Matheus Grossi <thebravematteo33@protonmail.com>
2024-04-03 12:32:36 +0200

    fixing things

--- src/components/Other/index.js
+++ src/components/Other/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import aboutMe from "../../Assets/aboutMe.png";
-import Tilt from "react-parallax-tilt";
-import Particle from "../Particle";
 
 
 
@@ -15,19 +12,14 @@ function Other() {
       <Container>
         <Row>
           <Col md={8} className="home-about-description">
-           <h1>Eu a patroa e as crianças</h1>
+           <h1>Eu a patroa e as crianças - 24 horas</h1>
           <div>
-          <iframe src='https://widgets.sociablekit.com/twitch-live-videos/iframe/25392232' frameborder='0' width='100%' height='1000'></iframe>
+          <iframe autoplay="false" muted="false" src='https://widgets.sociablekit.com/twitch-live-videos/iframe/25392232' frameborder='0' width='100%' height='1000' allowFullScreen ></iframe>
           </div>
-          {/* <h1>Todo mundo odeia o Chris</h1>
-          <iframe src='https://widgets.sociablekit.com/twitch-live-videos/iframe/25392238' frameborder='0' width='100%' height='1000'></iframe> */}
+          <h1>Todo mundo odeia o Chris - 24 oras </h1>
+          <iframe autoplay="false" muted="true" src='https://widgets.sociablekit.com/twitch-live-videos/iframe/25392238' frameborder='0' width='100%' height='1000' allowFullScreen ></iframe>
           </Col>
-          {/* <Col md={4} className="myAvatar">
-            <Tilt>
-              <img src={aboutMe} className="img-fluid" alt="avatar"
-              style={{ height: "200px" }} />
-            </Tilt>
-          </Col> */}
+        
         </Row>
         <Row>
          
