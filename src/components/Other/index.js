1c5f2abab26d4a90fee8b3bc6038294d5569df8e
Matheus Grossi <thebravematteo33@protonmail.com>
2024-04-05 17:04:49 +0200

    getting tired

--- src/components/Other/index.js
+++ src/components/Other/index.js
@@ -15,7 +15,7 @@ function Other() {
            <h1>Eu a patroa e as crianças - 24 horas</h1>
           <div>
           {/* <iframe autoplay="false" muted="false" src='https://widgets.sociablekit.com/twitch-live-videos/iframe/25392232' frameborder='0' width='100%' height='1000' allowFullScreen ></iframe> */}
-          <iframe src="https://player.twitch.tv/?channel=euapatroaeascrias&parent=matteo-portfolio-matteothebrave.bohr.io/" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+          <iframe src="https://player.twitch.tv/?channel=euapatroaeascrias&parent=matteo-portfolio-matteothebrave.bohr.io/other" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
           <div id="twitch-embed">Twitch</div>
           </div>
           <h1>Todo mundo odeia o Chris - 24 horas </h1>
