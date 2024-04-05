6ff47f5a4f7d35dd0b937714a293bc124a0d4fca
Matheus Grossi <thebravematteo33@protonmail.com>
2024-04-05 17:07:16 +0200

    getting tired

--- src/components/Other/index.js
+++ src/components/Other/index.js
@@ -16,7 +16,6 @@ function Other() {
           <div>
           {/* <iframe autoplay="false" muted="false" src='https://widgets.sociablekit.com/twitch-live-videos/iframe/25392232' frameborder='0' width='100%' height='1000' allowFullScreen ></iframe> */}
           <iframe src="https://player.twitch.tv/?channel=euapatroaeascrias&parent=matteo-portfolio-matteothebrave.bohr.io/other" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
-          <div id="twitch-embed">Twitch</div>
           </div>
           <h1>Todo mundo odeia o Chris - 24 horas </h1>
           <iframe autoplay="false" muted="true" src='https://widgets.sociablekit.com/twitch-live-videos/iframe/25392238' frameborder='0' width='100%' height='1000' allowFullScreen ></iframe>
