e9b1ad17b472487db95216a032f0c4a61e6058eb
Matheus Grossi <thebravematteo33@protonmail.com>
2024-05-21 12:00:28 +0200

    removing secret section

--- src/components/Secret/Secret.js
+++ src/components/Secret/Secret.js
@@ -1,35 +1,35 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+// import React from "react";
+// import { Container, Row, Col } from "react-bootstrap";
 
 
 
-function Secret() {
+// function Secret() {
  
-  return (
+//   return (
     
       
-    <Container fluid className="home-about-section" id="about">
-      <Container>
-        <Row>
-          <Col md={8} className="home-about-description">
-           <h1>Eu a patroa e as crianças - 24 horas</h1>
-          <div>
-          {/* <iframe autoplay="false" muted="false" src='https://widgets.sociablekit.com/twitch-live-videos/iframe/25392232' frameborder='0' width='100%' height='1000' allowFullScreen ></iframe>
-          This is not being used, but i will let here anyway
-           */}
-          <iframe src="https://player.twitch.tv/?channel=euapatroaeascrias&parent=matteo-portfolio-matteothebrave.bohr.io" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
-          </div>
-          <h1>Todo mundo odeia o Chris - 24 horas </h1>
-          <iframe src="https://player.twitch.tv/?channel=todomundoultrahd&parent=matteo-portfolio-matteothebrave.bohr.io" frameborder="0" allowfullscreen="true" muted="true" autoplay="false" height="378" width="620"></iframe>
-          </Col>
+//     <Container fluid className="home-about-section" id="about">
+//       <Container>
+//         <Row>
+//           <Col md={8} className="home-about-description">
+//            <h1>Eu a patroa e as crianças - 24 horas</h1>
+//           <div>
+//           {/* <iframe autoplay="false" muted="false" src='https://widgets.sociablekit.com/twitch-live-videos/iframe/25392232' frameborder='0' width='100%' height='1000' allowFullScreen ></iframe>
+//           This is not being used, but i will let here anyway
+//            */}
+//           <iframe src="https://player.twitch.tv/?channel=euapatroaeascrias&parent=matteo-portfolio-matteothebrave.bohr.io" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+//           </div>
+//           <h1>Todo mundo odeia o Chris - 24 horas </h1>
+//           <iframe src="https://player.twitch.tv/?channel=todomundoultrahd&parent=matteo-portfolio-matteothebrave.bohr.io" frameborder="0" allowfullscreen="true" muted="true" autoplay="false" height="378" width="620"></iframe>
+//           </Col>
         
-        </Row>
-        <Row>
+//         </Row>
+//         <Row>
          
-        </Row>
-      </Container>
-    </Container>
-  );
-}
+//         </Row>
+//       </Container>
+//     </Container>
+//   );
+// }
 
-export default Secret;
+// export default Secret;
